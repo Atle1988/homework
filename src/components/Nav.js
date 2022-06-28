bea15399ecfa75faff34c19eb696f73bbf9fb5e1
Atle1988 <101109273+Atle1988@users.noreply.github.com>
2022-06-28 17:28:57 +0200

    update navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,15 @@
+import navIcon from "../bilder/navIcon.png"
+
 export default function Nav(){
     return(
-        <nav>
-            <ul>
-            <li>home</li>
-            <li>about me</li>
-            </ul>
-        </nav>
+        <div className="flex--nav-box">
+            <img className="nav--img" src={navIcon} alt="nav--icon" />
+            <div className="flex--nav-link">
+                <a href="#home" >Home</a>
+                <a href="#omgMEg" >Om meg</a>
+                <a href="#skills" >Skills</a>
+                <a href="#prosjekter" >Prosjekter</a>
+            </div>
+        </div>
     )
 }
